1ce602ba04aeec244b5c877e40daeedc2c35f804
Maxximiliana <dsmaximiliana@gmail.com>
2015-05-03 18:06:43 -0300

    Mapeo TipoSocio y EstadoCivil

--- github.com/CatemDesa/CATEM/ProyectCATEM/src/ar/com/catem/model/TipoSocio.java
+++ github.com/CatemDesa/CATEM/ProyectCATEM/src/ar/com/catem/model/TipoSocio.java
@@ -1,22 +1,37 @@
 package ar.com.catem.model;
 
-public class TipoSocio {
+import java.io.Serializable;
 
-	private Integer idTipoSocio;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TIPO_SOCIO")
+public class TipoSocio implements Serializable{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2860257088388598233L;
+		@Column(name="IDTIPOSOCIO")
+	@GeneratedValue
+	@Id
+	private Integer  idTipoSocio;
+	@Column(name="DESCRIPCION")
 	private String descripcion;
-
 	public Integer getIdTipoSocio() {
 		return idTipoSocio;
 	}
-
 	public void setIdTipoSocio(Integer idTipoSocio) {
 		this.idTipoSocio = idTipoSocio;
 	}
-
 	public String getDescripcion() {
 		return descripcion;
 	}
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
